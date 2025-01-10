56bb28b72c3a1f872a773ce80a2e86d13af0379c
dekuofa1995 <56107353+dekuofa1995@users.noreply.github.com>
2025-01-10 15:39:06 +0800

    feat: click and pop a modal for image

--- components/Sidebar/OrgImage.tsx
+++ components/Sidebar/OrgImage.tsx
@@ -1,90 +1,104 @@
 /* eslint-disable @next/next/no-img-element */
-import React, { useState } from 'react'
+import React, { ElementRef, useState, useRef, useEffect } from 'react'
 import Image from 'next/image'
 import path from 'path'
 import { Container } from '@chakra-ui/react'
+import { createPortal } from 'react-dom'
 //import '../../../public/placeholder.png'
 
 const convertToNextImageAttr = (value: string | number | undefined): number | undefined => {
-  if (value === undefined) {
-    return undefined
-  }
+	if (value === undefined) {
+		return undefined
+	}
 
-  if (typeof value === 'number' || !isNaN(Number(value))) {
-    return Number(value)
-  }
+	if (typeof value === 'number' || !isNaN(Number(value))) {
+		return Number(value)
+	}
 
-  if (value.endsWith('px')) {
-    return Number(value.replace('px', ''))
-  }
+	if (value.endsWith('px')) {
+		return Number(value.replace('px', ''))
+	}
 
-  return undefined
+	return undefined
 }
 
 export interface OrgImageProps {
-  org: React.ClassAttributes<HTMLImageElement> & React.ImgHTMLAttributes<HTMLImageElement>
-  file: string
+	org: React.ClassAttributes<HTMLImageElement> & React.ImgHTMLAttributes<HTMLImageElement>
+	file: string
 }
 
 export const OrgImage = (props: OrgImageProps) => {
-  const { org, file } = props
-  const { src, ...attr } = org
+	const { org, file } = props
+	const { src, ...attr } = org
+	const [open, setOpen] = useState(false)
+	const dialogRef = useRef<ElementRef<'dialog'>>(null)
+	useEffect(() => {
+		if (open) {
+			dialogRef.current?.showModal()
+		} else {
+			dialogRef.current?.close()
+		}
+	}, [open])
 
-  if (!src) {
-    return null
-  }
+	if (!src) {
+		return null
+	}
 
-  const dumbLoader = ({ src, width, quality }: { [key: string]: string | number }) => {
-    return `${src}`
-  }
-  const homeLoader = ({ src, width, quality }: { [key: string]: string | number }) => {
-    return `img/${src}`
-  }
+	const dumbLoader = ({ src, width, quality }: { [key: string]: string | number }) => {
+		return `${src}`
+	}
+	const homeLoader = ({ src, width, quality }: { [key: string]: string | number }) => {
+		return `img/${src}`
+	}
 
-  if (src?.startsWith("file:")) {
-    const srcName = src.replace(/file:/g, './')
-    const attrWidth = convertToNextImageAttr(attr.width)
-    const attrHeight = convertToNextImageAttr(attr.height)
-    const layout = attrWidth || attrHeight ? undefined : "responsive"
-    return (
-      <Image
-        {...attr}
-        layout={layout}
-        loader={dumbLoader}
-        src={srcName}
-        width={attrWidth}
-        height={attrHeight}
-        alt={attr.alt ?? "Wow, an image."}
-      />
-    )
-  }
+	const attrWidth = convertToNextImageAttr(attr.width)
+	const attrHeight = convertToNextImageAttr(attr.height)
+	const layout = attrWidth || attrHeight ? undefined : 'responsive'
+	if (/http.*/g.test(src.replace(/(http)?.*/g, '$1'))) {
+		return (
+			<Image
+				{...attr}
+				layout={layout}
+				loader={dumbLoader}
+				src={src}
+				width={attrWidth}
+				height={attrHeight}
+				alt={attr.alt ?? 'Wow, an image.'}
+			/>
+		)
+	}
 
-  if (/(http)?.*/g.test(src.replace(/(http)?.*/g, '$1'))) {
-    const attrWidth = convertToNextImageAttr(attr.width)
-    const attrHeight = convertToNextImageAttr(attr.height)
-    const layout = attrWidth || attrHeight ? undefined : "responsive"
-    return (
-      <Image
-        {...attr}
-        layout={layout}
-        loader={dumbLoader}
-        src={src}
-        width={attrWidth}
-        height={attrHeight}
-        alt={attr.alt ?? "Wow, an image."}
-      />
-    )
-  }
+	const srcName = src.replace(/file:/g, '')
 
-  const srcName = src.replace(/file:/g, '')
+	const dir = path.dirname(file)
+	const fullPath =
+		path.isAbsolute(srcName) || srcName.slice(0, 1) === '~' ? srcName : path.join(dir, srcName)
+	// const encodedPath = encodeURIComponent(encodeURIComponent(fullPath))
 
-  const dir = path.dirname(file)
-  const fullPath =
-    path.isAbsolute(srcName) || srcName.slice(0, 1) === '~' ? srcName : path.join(dir, srcName)
-  const encodedPath = encodeURIComponent(encodeURIComponent(fullPath))
-  return (
-    <Container my={4} position="relative">
-      <img {...attr} alt={attr.alt ?? "Wow, an image."} src={fullPath} />
-    </Container>
-  )
+	return (
+		<>
+			{open &&
+				createPortal(
+					<div className="modal-backdrop" onClick={() => setOpen(false)}>
+						<dialog ref={dialogRef} className="modal" onClose={() => setOpen(false)}>
+							<img
+								{...attr}
+								alt={attr.alt ?? 'Wow, an image.'}
+								src={fullPath}
+								onClick={() => setOpen(true)}
+							/>
+						</dialog>
+					</div>,
+					document.getElementById('modal-root') as HTMLElement,
+				)}
+			<Container my={4} position="relative">
+				<img
+					{...attr}
+					alt={attr.alt ?? 'Wow, an image.'}
+					src={fullPath}
+					onClick={() => setOpen(true)}
+				/>
+			</Container>
+		</>
+	)
 }
