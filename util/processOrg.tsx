95f75452efbe52ab002cd8203da6b8db8e75a88f
dekuofa1995 <56107353+dekuofa1995@users.noreply.github.com>
2025-01-10 15:44:36 +0800

    feat: only add backgrond color for inline code

--- util/processOrg.tsx
+++ util/processOrg.tsx
@@ -8,7 +8,7 @@ import attachments from 'uniorg-attach'
 // rehypeHighlight does not have any types
 // add error thing here
 // import highlight from 'rehype-highlight'
-import rehypePrism from '@mapbox/rehype-prism';
+import rehypePrism from '@mapbox/rehype-prism'
 import katex from 'rehype-katex'
 import 'katex/dist/katex.css'
 import rehype2react from 'rehype-react'
@@ -36,191 +36,195 @@ import { OrgRoamLink, OrgRoamNode } from '../api'
 
 // @ts-expect-error non-ESM unified means no types
 import { toString } from 'hast-util-to-string'
-import { Box, chakra } from '@chakra-ui/react'
+import { Box, chakra, Code } from '@chakra-ui/react'
 import { normalizeLinkEnds } from './normalizeLinkEnds'
 import * as prod from 'react/jsx-runtime'
 
 export interface ProcessedOrgProps {
-  nodeById: NodeById
-  previewNode: OrgRoamNode
-  setPreviewNode: any
-  previewText: any
-  nodeByCite: NodeByCite
-  setSidebarHighlightedNode: any
-  openContextMenu: any
-  outline: boolean
-  collapse: boolean
-  linksByNodeId: LinksByNodeId
-  macros: { [key: string]: string } | {}
-  attachDir: string
-  useInheritance: boolean
+	nodeById: NodeById
+	previewNode: OrgRoamNode
+	setPreviewNode: any
+	previewText: any
+	nodeByCite: NodeByCite
+	setSidebarHighlightedNode: any
+	openContextMenu: any
+	outline: boolean
+	collapse: boolean
+	linksByNodeId: LinksByNodeId
+	macros: { [key: string]: string } | {}
+	attachDir: string
+	useInheritance: boolean
 }
 
 export const ProcessedOrg = (props: ProcessedOrgProps) => {
-  const {
-    nodeById,
-    setSidebarHighlightedNode,
-    setPreviewNode,
-    previewText,
-    nodeByCite,
-    previewNode,
-    openContextMenu,
-    outline,
-    collapse,
-    linksByNodeId,
-    macros,
-    attachDir,
-    useInheritance,
-  } = props
+	const {
+		nodeById,
+		setSidebarHighlightedNode,
+		setPreviewNode,
+		previewText,
+		nodeByCite,
+		previewNode,
+		openContextMenu,
+		outline,
+		collapse,
+		linksByNodeId,
+		macros,
+		attachDir,
+		useInheritance,
+	} = props
 
-  const orgProcessor = unified()
-    .use(uniorgParse)
-    .use(extractKeywords)
-    .use(attachments, {
-      idDir: attachDir || undefined,
-      useInheritance,
-    })
-    .use(uniorgSlug)
-    .use(uniorg2rehype, { useSections: true })
+	const orgProcessor = unified()
+		.use(uniorgParse)
+		.use(extractKeywords)
+		.use(attachments, {
+			idDir: attachDir || undefined,
+			useInheritance,
+		})
+		.use(uniorgSlug)
+		.use(uniorg2rehype, { useSections: true })
 
-  const nodesInNote =
-    linksByNodeId[previewNode?.id!]?.reduce((acc: NodeById, link: OrgRoamLink) => {
-      const links = normalizeLinkEnds(link)
-      const relevantLink = links.filter((l) => l !== previewNode.id).join('')
-      return {
-        ...acc,
-        [relevantLink]: nodeById[relevantLink],
-      }
-    }, {}) || {}
+	const nodesInNote =
+		linksByNodeId[previewNode?.id!]?.reduce((acc: NodeById, link: OrgRoamLink) => {
+			const links = normalizeLinkEnds(link)
+			const relevantLink = links.filter((l) => l !== previewNode.id).join('')
+			return {
+				...acc,
+				[relevantLink]: nodeById[relevantLink],
+			}
+		}, {}) || {}
 
-  const linkEntries = Object.entries(nodesInNote)
-  const wikiLinkResolver = (wikiLink: string): string[] => {
-    const entry = linkEntries.find((idNodeArray) => {
-      return idNodeArray?.[1]?.title === wikiLink
-    })
-    const id = entry?.[0] ?? ''
-    return [id]
-  }
+	const linkEntries = Object.entries(nodesInNote)
+	const wikiLinkResolver = (wikiLink: string): string[] => {
+		const entry = linkEntries.find((idNodeArray) => {
+			return idNodeArray?.[1]?.title === wikiLink
+		})
+		const id = entry?.[0] ?? ''
+		return [id]
+	}
 
-  const wikiLinkProcessor = (wikiLink: string): string => {
-    return `id:${wikiLink}`
-  }
+	const wikiLinkProcessor = (wikiLink: string): string => {
+		return `id:${wikiLink}`
+	}
 
-  const mdProcessor = unified()
-    .use(remarkParse)
-    .use(remarkFrontMatter, ['yaml'])
-    .use(remarkExtractFrontMatter)
-    .use(remarkWikiLinks, {
-      permaLinks: Object.keys(nodesInNote),
-      pageResolver: wikiLinkResolver,
-      hrefTemplate: wikiLinkProcessor,
-    })
-    .use(remarkSectionize)
-    .use(remarkMath)
-    .use(remarkGFM)
-    .use(remarkRehype)
-  //.data('settings', { fragment: true })
-  // .use(highlight)
+	const mdProcessor = unified()
+		.use(remarkParse)
+		.use(remarkFrontMatter, ['yaml'])
+		.use(remarkExtractFrontMatter)
+		.use(remarkWikiLinks, {
+			permaLinks: Object.keys(nodesInNote),
+			pageResolver: wikiLinkResolver,
+			hrefTemplate: wikiLinkProcessor,
+		})
+		.use(remarkSectionize)
+		.use(remarkMath)
+		.use(remarkGFM)
+		.use(remarkRehype)
+	//.data('settings', { fragment: true })
+	// .use(highlight)
 
-  const isMarkdown = previewNode?.file?.slice(-3) === '.md'
-  // const baseProcessor = isMarkdown ? mdProcessor : orgProcessor
+	const isMarkdown = previewNode?.file?.slice(-3) === '.md'
+	// const baseProcessor = isMarkdown ? mdProcessor : orgProcessor
 
-  const processor = useMemo(
-    () =>
-      orgProcessor
-        .use(katex, {
-          trust: (context) => ['\\htmlId', '\\href'].includes(context.command),
-          macros: {
-            '\\eqref': '\\href{###1}{(\\text{#1})}',
-            '\\ref': '\\href{###1}{\\text{#1}}',
-            '\\label': '\\htmlId{#1}{}',
-            // '\\weird': '\\textbf{#1}',
-            ...macros,
-          },
-        })
-        // @ts-ignore
-        .use(rehype2react, {
-          Fragment: prod.Fragment,
-          jsx: prod.jsx,
-          jsxs: prod.jsxs,
-          createElement: React.createElement,
-          components: {
-            a: ({ children, href }) => {
-              return (
-                <PreviewLink
-                  nodeByCite={nodeByCite}
-                  setSidebarHighlightedNode={setSidebarHighlightedNode}
-                  href={`${href as string}`}
-                  nodeById={nodeById}
-                  linksByNodeId={linksByNodeId}
-                  setPreviewNode={setPreviewNode}
-                  openContextMenu={openContextMenu}
-                  outline={outline}
-                  previewNode={previewNode}
-                  isWiki={isMarkdown}
-                  macros={macros}
-                  attachDir={attachDir}
-                  useInheritance={useInheritance}
-                >
-                  {children}
-                </PreviewLink>
-              )
-            },
-            img: (org) => {
-              return <OrgImage org={org} file={previewNode?.file} />
-            },
-            section: ({ children, className }) => {
-              if (className && className?.slice(-1) === `${previewNode.level}`) {
-                return <Box>{children}</Box>
-              }
-              return (
-                <Section {...{ outline, collapse }} className={className ?? ""}>
-                  {children}
-                </Section>
-              )
-            },
-            blockquote: ({ children }) => (
-              <chakra.blockquote
-                color="gray.800"
-                bgColor="gray.300"
-                pt={4}
-                pb={2}
-                mb={4}
-                mt={3}
-                pl={4}
-                borderLeftWidth={4}
-                borderLeftColor="gray.700"
-              >
-                {children}
-              </chakra.blockquote>
-            ),
-            p: ({ children }) => {
-              return <p lang="en">{children as ReactNode}</p>
-            },
-            code: ({ children }) => {
-              return <chakra.code
-                px={1}
-                py={0.5}
-                rounded="sm"
-                fontSize="xs"
-                color="gray.800"
-                bgColor="gray.300"
-              >
-                {children}
-              </chakra.code>;
-            },
-          },
-        })
-        .use(rehypePrism as any, { ignoreMissing: true }),
-    [previewNode?.id],
-  )
+	const processor = useMemo(
+		() =>
+			orgProcessor
+				.use(katex, {
+					trust: (context) => ['\\htmlId', '\\href'].includes(context.command),
+					macros: {
+						'\\eqref': '\\href{###1}{(\\text{#1})}',
+						'\\ref': '\\href{###1}{\\text{#1}}',
+						'\\label': '\\htmlId{#1}{}',
+						// '\\weird': '\\textbf{#1}',
+						...macros,
+					},
+				})
+				// @ts-ignore
+				.use(rehype2react, {
+					Fragment: prod.Fragment,
+					jsx: prod.jsx,
+					jsxs: prod.jsxs,
+					createElement: React.createElement,
+					components: {
+						a: ({ children, href }) => {
+							return (
+								<PreviewLink
+									nodeByCite={nodeByCite}
+									setSidebarHighlightedNode={setSidebarHighlightedNode}
+									href={`${href as string}`}
+									nodeById={nodeById}
+									linksByNodeId={linksByNodeId}
+									setPreviewNode={setPreviewNode}
+									openContextMenu={openContextMenu}
+									outline={outline}
+									previewNode={previewNode}
+									isWiki={isMarkdown}
+									macros={macros}
+									attachDir={attachDir}
+									useInheritance={useInheritance}
+								>
+									{children}
+								</PreviewLink>
+							)
+						},
+						img: (org) => {
+							return <OrgImage org={org} file={previewNode?.file} />
+						},
+						section: ({ children, className }) => {
+							if (className && className?.slice(-1) === `${previewNode.level}`) {
+								return <Box>{children}</Box>
+							}
+							return (
+								<Section {...{ outline, collapse }} className={className ?? ''}>
+									{children}
+								</Section>
+							)
+						},
+						blockquote: ({ children }) => (
+							<chakra.blockquote
+								color="gray.800"
+								bgColor="gray.300"
+								pt={4}
+								pb={2}
+								mb={4}
+								mt={3}
+								pl={4}
+								borderLeftWidth={4}
+								borderLeftColor="gray.700"
+							>
+								{children}
+							</chakra.blockquote>
+						),
+						p: ({ children }) => {
+							return <p lang="en">{children as ReactNode}</p>
+						},
+						code: ({ children, className }) => {
+							const isInlineCode = className?.includes('inline-code')
+							return (
+								<chakra.code
+									className={className}
+									px={1}
+									py={0.5}
+									rounded="sm"
+									fontSize="xs"
+									color="gray.800"
+									bgColor={isInlineCode ? 'gray.300' : 'none'}
+								>
+									{children}
+								</chakra.code>
+							)
+						},
+					},
+				})
+				.use(rehypePrism as any, { ignoreMissing: true }),
+		[previewNode?.id],
+	)
 
-  const text = useMemo(() => processor.processSync(previewText).result, [previewText])
-  if (!previewNode || !linksByNodeId) {
-    return null
-  }
+	const text = useMemo(() => processor.processSync(previewText).result, [previewText])
+	if (!previewNode || !linksByNodeId) {
+		return null
+	}
 
-  return (
-    <NoteContext.Provider value={{ collapse, outline }}>{text as ReactNode}</NoteContext.Provider>
-  )
+	return (
+		<NoteContext.Provider value={{ collapse, outline }}>{text as ReactNode}</NoteContext.Provider>
+	)
 }
